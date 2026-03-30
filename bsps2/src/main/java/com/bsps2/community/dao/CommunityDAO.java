0052ec67e8d44b02dee3c690fb0bb488d12102a4
User <User@HYW>
2026-03-30 20:31:54 +0900

    글삭제

--- bsps2/src/main/java/com/bsps2/community/dao/CommunityDAO.java
+++ bsps2/src/main/java/com/bsps2/community/dao/CommunityDAO.java
@@ -152,4 +152,25 @@ public class CommunityDAO extends DAO {
 		
 	}//write()의 끝
 	
+	//4.글수정
+	
+	//5.글삭제
+	public Integer delete(CommunityVO vo) throws Exception{
+		Integer result = 0;
+	
+		//1.드라이버 확인 2.연결
+		con = DB.getConnection();
+		//3.SQL작성
+		String sql = "delete from community where no = ? and pw = ?";
+		//4.실행 객체 및 데이터 세팅
+		pstmt = con.prepareStatement(sql);
+		pstmt.setLong(1, vo.getNo());
+		pstmt.setString(2, vo.getPw());
+		//5.실행,6.결과 담기
+		result = pstmt.executeUpdate();
+		
+		//7.닫기
+		DB.close(con, pstmt);
+		return result;
+	}//delete()의 끝
 }
