6661b85b7a7e14091a5ae8e6694bd2861e1bad9e
User <User@HYW>
2026-03-30 20:52:07 +0900

    .

---
+++ bsps2/src/main/java/com/bsps2/community/service/CommunityDeleteService.java
@@ -0,0 +1,25 @@
+package com.bsps2.community.service;
+
+import com.bsps2.community.dao.CommunityDAO;
+import com.bsps2.community.vo.CommunityVO;
+import com.bsps2.main.dao.DAO;
+import com.bsps2.main.service.Service;
+
+public class CommunityDeleteService implements Service {
+
+	private CommunityDAO dao;
+	
+	@Override
+	public void setDAO(DAO dao) {
+		// TODO Auto-generated method stub
+		this.dao = (CommunityDAO) dao;
+		
+	}
+
+	@Override
+	public Object service(Object obj) throws Exception {
+		// TODO Auto-generated method stub
+		return dao.delete((CommunityVO)obj);
+	}
+
+}
