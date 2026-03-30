de72673717c5e404af28e25e8843ebb8522fa322
User <User@HYW>
2026-03-30 19:36:07 +0900

    글등록 처리

--- bsps2/src/main/java/com/bsps2/community/controller/CommunityController.java
+++ bsps2/src/main/java/com/bsps2/community/controller/CommunityController.java
@@ -1,5 +1,6 @@
 package com.bsps2.community.controller;
 
+import com.bsps2.community.vo.CommunityVO;
 import com.bsps2.main.controller.Controller;
 import com.bsps2.main.controller.Init;
 import com.bsps2.main.service.Execute;
@@ -37,8 +38,22 @@ public class CommunityController implements Controller {
 				request.setAttribute("vo", Execute.execute(Init.getService(uri), new Long[] {no,inc}));				
 				return "community/view";
 				
-			//3.글등록
+			//3-1.글등록 폼
+			case "/community/writeForm.do":
+				return "community/writeForm";
 			
+			//3-2 글등록 처리
+			case "/community/write.do":
+				CommunityVO vo = new CommunityVO();
+				vo.setTitle(request.getParameter("title"));
+				vo.setContent(request.getParameter("content"));
+				vo.setWriter(request.getParameter("writer"));
+				vo.setPw(request.getParameter("pw"));
+				vo.setFileName(request.getParameter("fileName"));
+				
+				Execute.execute(Init.getService(uri), vo);
+				session.setAttribute("msg", "새로운 제보 글이 등록되었습니다.");
+				return "redirect:list.do?perPageNum=" + request.getParameter("perPageNum");
 				
 				
 			default:
