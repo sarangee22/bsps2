2eaa05adb670a413a0d1f973e788c9db1351a4e4
User <User@HYW>
2026-04-03 20:54:16 +0900

    권한처리 생성

--- bsps2/src/main/java/com/bsps2/util/filter/AuthorityFilter.java
+++ bsps2/src/main/java/com/bsps2/util/filter/AuthorityFilter.java
@@ -16,6 +16,10 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.bsps2.member.vo.LoginVO;
+
+
+
 
 /**
  * Servlet Filter implementation class AutorityFilter
@@ -60,8 +64,36 @@ public class AuthorityFilter extends HttpFilter implements Filter {
 		System.out.println("AutorityFilter.doFilter() 실행전 처리 : 권한 처리 ----------------------");
 
 		Integer pageGradeNo = authMap.get(uri);
-		chain.doFilter(request,response);
 		
+		if(pageGradeNo != null) {
+			// 사용자가 로그인 처리가 되어 있어야만 한다.
+			// 1. 로그인 정보를 가져온다.
+			HttpSession session = req.getSession();
+			LoginVO login = (LoginVO) session.getAttribute("login");
+			// 2. 로그인이 안되었을 때 로그인 하라고 시킨다.
+			if(login == null) {
+				// 메시지 처리
+				session.setAttribute("msg", "로그인이 필요한 페이지입니다.");
+				res.sendRedirect("/member/loginForm.do");
+				return;
+			} // 로그인이 안되었을 때의 처리 끝
+			
+			// 로그인이 되었을 때의 관리자 처리
+			if(pageGradeNo == 9) {
+				// 관리자 권한이 아닌 경우의 처리 - 일반 회원인 경우(1)
+				if(login.getGradeNo() < pageGradeNo) {
+					req.setAttribute("url", req.getRequestURL());
+					req.getRequestDispatcher("/WEB-INF/views/error/auth.jsp").forward(req, res);
+					return;
+				} // 관리자 권한 if의 끝
+			} // 페이지의 권한이 관리자인 경우 끝
+			
+		} // if(pageGradeNo != null) 의 끝
+		
+		// pass the request along the filter chain
+		chain.doFilter(request, response); // 요청에 따른 실제적인 실행
+		
+		// 실행 후 필터 처리
 	}
 
 	/**
@@ -70,7 +102,38 @@ public class AuthorityFilter extends HttpFilter implements Filter {
 	 */
 	public void init(FilterConfig fConfig) throws ServletException {
 
+		System.out.println("AutorityFilter.init()-------------------");
 		
+		// --- [회원 관리] 권한 설정 ---
+		authMap.put("/member/view.do", 1);
+		authMap.put("/member/editForm.do", 1);
+		authMap.put("/member/edit.do", 1);
+		authMap.put("/member/changePwForm.do", 1);
+		authMap.put("/member/changePw.do", 1);
+		authMap.put("/member/deleteForm.do", 1);
+		authMap.put("/member/delete.do", 1);
+		
+		// 관리자 전용 회원 관리
+		authMap.put("/member/list.do", 9);
+		authMap.put("/member/changeStatus.do", 9);
+		authMap.put("/admin/main.do", 9);
+		
+		// --- [퀴즈 게시판] 권한 설정 ---
+		// 퀴즈 등록/수정/삭제는 오직 관리자(9)만 가능
+		authMap.put("/quiz/writeForm.do", 9);
+		authMap.put("/quiz/write.do", 9);
+		authMap.put("/quiz/updateForm.do", 9);
+		authMap.put("/quiz/update.do", 9);
+		authMap.put("/quiz/delete.do", 9);
+
+		// --- [제보 게시판(Community)] 권한 설정 ---
+		// 등록/수정/삭제는 로그인한 회원(1) 이상 가능
+		// (작성자 본인 확인은 Controller에서 추가로 진행)
+		authMap.put("/community/writeForm.do", 1);
+		authMap.put("/community/write.do", 1);
+		authMap.put("/community/updateForm.do", 1);
+		authMap.put("/community/update.do", 1);
+		authMap.put("/community/delete.do", 1);
 	}
 
 }
